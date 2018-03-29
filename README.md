ab305de0bf6c267d59d433137f546e90b86e1982
VeranikaBuzhynskaya <fruittelllla@gmail.com>
2018-03-29 23:36:45 +0300

    Implement react task

---
+++ README.md
@@ -0,0 +1,9 @@
+# reactFrontCamp
+
+#### For start project:
+1. npm i;
+2. npm run build-dev (or npm run build-prod);
+3. npm start;
+
+#### For tests:
+1. npm test
---
+++ app/__test__/ContentPost.test.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { withRouter, BrowserRouter, Link } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import ContentPost from '../client/components/Content/ContentPost';
+import Post from '../client/components/Post/Post';
+
+
+import data from './mockDataOfPosts';
+import storeConstructor from '../client/store';
+
+const store = storeConstructor({});
+
+describe('Content', () => {
+    debugger;
+    let content;
+    beforeAll(() => {
+        debugger;
+        store.dispatch({
+            type:'RECIEVE_POSTS',
+            posts: data
+        });
+        debugger;
+        // content = mount(
+        //     <BrowserRouter>
+        //     <Provider>
+                
+        //     </Provider>
+        // </BrowserRouter>
+        // );
+        content = mount(
+            <BrowserRouter>
+            <div>
+                
+            </div>
+        </BrowserRouter>
+        );
+        
+    //     <BrowserRouter>
+    //     <Provider store={store}>
+    //         <ContentPost/>
+    //     </Provider>
+    // </BrowserRouter>
+        console.log('content', content);
+    });
+    debugger;
+    it('render all recieved items', () => {
+    debugger;
+        expect(content.find(Post).length)
+            .toEqual(8);
+
+    });
+})
